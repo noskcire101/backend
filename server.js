923c29096d82cd62cef4ade22251e3383cee69de
noskcire101 <esernero@gmail.com>
2023-10-31 17:17:45 +0800

    installed nodemon and dotenv

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const express = require("express");
 
 //express app
 const app = express();
 
+// routes
+app.get('/',(req, res) => {
+    res.json({'Welcome to the app'});
+});
+
 //listen for requests
-app.listen(4000, () => {
-    console.log("Server is listening on port 4000");
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening on port 4000!!");
 });
